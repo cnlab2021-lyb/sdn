22c4ed11d8f4954b3062d614f9963f1679903ad3
TaWeiTu <tu.da.wei@gmail.com>
2021-05-21 07:04:22 -0700

    Refactor network slicing
    - Add rule to the switch that drops packets instead of notifying the
    controller every time.

--- controller/controller.py
+++ controller/controller.py
@@ -15,22 +15,80 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto.ofproto_v1_3 import OXM_OF_IPV4_SRC, OXM_OF_IPV4_DST
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
-from ryu.lib.packet import ipv4
+from ryu.lib.packet import ipv4, ipv6
 from ryu.lib.packet import arp
+from ryu.lib import hub
 
 
-class SimpleSwitch13(app_manager.RyuApp):
+class Switch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        super(Switch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+
+    def _send_desc_reqeust(self, datapath):
+        ofp_parser = datapath.ofproto_parser
+        req = ofp_parser.OFPDescStatsRequest(datapath, 0)
+        datapath.send_msg(req)
+
+
+
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.logger.debug('register datapath: %016x', datapath.id)
+                self.datapaths[datapath.id] = datapath
+                # self._send_desc_request(datapath)
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.debug('unregister datapath: %016x', datapath.id)
+                del self.datapaths[datapath.id]
+        
+    def _request_stat(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        # match = parser.OFPMatch(ipv4_src=None, ipv4_dst=None, tcp_src=None, udp_src=None)
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+    def _monitor(self):
+        while True:
+            for dp in self.datapaths.values():
+                self._request_stat(dp)
+            hub.sleep(10)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        print(body)
+
+        # self.logger.info('datapath         '
+        #                  'in-port  eth-dst           '
+        #                  'out-port packets  bytes')
+        # self.logger.info('---------------- '
+        #                  '-------- ----------------- '
+        #                  '-------- -------- --------')
+        # for stat in sorted([flow for flow in body if flow.priority == 1],
+        #                    key=lambda flow: (flow.match['in_port'],
+        #                                      flow.match['eth_dst'])):
+        #     self.logger.info('%016x %8x %17s %8x %8d %8d',
+        #                      ev.msg.datapath.id,
+        #                      stat.match['in_port'], stat.match['eth_dst'],
+        #                      stat.instructions[0].actions[0].port,
+        #                      stat.packet_count, stat.byte_count)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -65,6 +123,14 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
+    def drop_packets(self, datapath, priority, match):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_CLEAR_ACTIONS, [])]
+        msg = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+        datapath.send_msg(msg)
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
@@ -80,30 +146,27 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
-        arp_pkt = pkt.get_protocol(arp.arp)
         ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
 
+        dst = eth.dst
+        src = eth.src
+
+        match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+        drop = False
         if ipv4_pkt:
             vlan_src = int(ipv4_pkt.src.split('.')[3]) % 2
             vlan_dst = int(ipv4_pkt.dst.split('.')[3]) % 2
-            if vlan_src != vlan_dst:
-                return
-        if arp_pkt:
-            vlan_src = int(arp_pkt.src_ip.split('.')[3]) % 2
-            vlan_dst = int(arp_pkt.dst_ip.split('.')[3]) % 2
-            if vlan_src != vlan_dst:
-                return
+            drop = vlan_src != vlan_dst
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
-        dst = eth.dst
-        src = eth.src
 
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        if not pkt.get_protocol(ipv6.ipv6):
+            self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -117,18 +180,21 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
-            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                return
+            if drop:
+                self.drop_packets(datapath, 1, match)
             else:
-                self.add_flow(datapath, 1, match, actions)
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    return
+                else:
+                    self.add_flow(datapath, 1, match, actions)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        if not drop:
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                      in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
